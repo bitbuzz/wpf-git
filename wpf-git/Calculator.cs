5b116d555a3a1fe956255e6eb5ae1d3384b3c3ae
bitbuzz <alexmuleh@gmail.com>
2016-10-26 19:12:07 -0600

    Added Calculator.cs

---
+++ wpf-git/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_git
+{
+	public class Calculator
+	{
+		public int Add(List<int> values)
+		{
+			int total = 0;
+			foreach(var i in values)
+			{
+				total += i;
+			}
+			return total;
+		}
+	}
+}
