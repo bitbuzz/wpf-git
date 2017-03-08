6dcadce5be0d495e2d5f53729651f7344d0cb9e0
bitbuzz <alexmuleh@gmail.com>
2017-03-08 16:16:43 -0700

    Math change
    One plus one is two.

--- wpf-git/CalculatorViewModel.cs
+++ wpf-git/CalculatorViewModel.cs
@@ -11,7 +11,7 @@ namespace wpf_git
 {
 	public class CalculatorViewModel : INotifyPropertyChanged
 	{
-		// changes comment.
+		// 1 + 1 = 2
 		public CalculatorViewModel()
 		{
 			DvrCtrlVisibility = Visibility.Visible;
